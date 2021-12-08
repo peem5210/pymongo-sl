9c1f995d7ee463a84d391bcc4b1f491a684f54f0
passawityakul <p.yakul@icloud.com>
2021-12-08 14:27:05 +0700

    [FEAT] Update one for pipeline rt

--- e2e_tests/update_many.py
+++ e2e_tests/update_many.py
@@ -2,4 +2,6 @@ from e2e_tests.validator import validate_result
 
 
 def validate_update_many(timing, _, collection_nt, collection_sl):
-    validate_result(timing, collection_nt.update_many, collection_sl.update_many, filter={"group": 2},update={'$set': {"read": True}})
+    undo_func = collection_nt.update_one
+    undo_kwargs = {"filter": {"group": 2}, "update": {'$set': {"read": False}}}
+    validate_result(timing, undo_func, undo_kwargs, collection_nt.update_many, collection_sl.update_many, filter={"group": 2}, update={'$set': {"read": True}})
